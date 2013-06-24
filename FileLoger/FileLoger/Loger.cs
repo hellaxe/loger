e082e8897c5311faed7f7ef12636151a162f023b
lis-san <fermindirak@gmail.com>
2013-06-24 11:52:19 +0300

    test
    simple test tool

---
+++ FileLoger/FileLoger/Loger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+
+namespace FileLoger
+{
+    internal class Loger
+    {
+        public Loger(string dirpath)
+        {
+            _path = dirpath;
+        }
+
+        public IList<FileInfo> FileList
+        {
+            get { return _fileList; }
+        } 
+
+        public void GetLog()
+        {
+            var dirInfo = new DirectoryInfo(_path);
+            _fileList = dirInfo.GetFiles();
+        }
+
+
+        public void SaveLog()
+        {
+            using (var file = new StreamWriter(_path + @"\log.txt"))
+            {
+                foreach (var fileInfo in _fileList)
+                {
+                    file.Write(fileInfo.Name + "\n");
+                }
+            }
+
+        }
+
+        private readonly string _path;
+        private IList<FileInfo> _fileList = new List<FileInfo>();
+    }
+}
---
+++ FileLoger/FileLoger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FileLoger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("FileLoger v. 1.0 ");
+                Console.WriteLine("Please enter the folder full path");
+                var path = Console.ReadLine();
+              
+                var loger = new Loger(path);
+                loger.GetLog();
+                loger.SaveLog();
+                Console.WriteLine("Press Enter to continue");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
